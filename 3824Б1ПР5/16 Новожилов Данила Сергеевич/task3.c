29a6bf1504cbaba972b617e836170f29d418617d
dsnovozhylov <d9162589@gmail.com>
2024-12-05 10:16:29 +0300

    Update task3.c

--- 3824Б1ПР5/16 Новожилов Данила Сергеевич/task3.c
+++ 3824Б1ПР5/16 Новожилов Данила Сергеевич/task3.c
@@ -3,3 +3,19 @@ double max_of_two_numbers(double a, double b)
 {
   return 0.0;
 }
+#include <stdio.h>
+
+double max_of_two_numbers(double a, double b) {
+    return (a > b) ? a : b;
+}
+
+int main() {
+    double a = 3.14;
+    double b = 6.28;
+    
+    double result = max_of_two_numbers(a, b);
+    
+    printf("Максимум из %.2f и %.2f равен %.2f\n", a, b, result);
+    
+    return 0;
+}
