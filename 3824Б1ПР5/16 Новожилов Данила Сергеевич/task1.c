973d9ea518fd9976e48ef48381a005553f477265
dsnovozhylov <d9162589@gmail.com>
2024-12-05 10:10:47 +0300

    Update task1.c

--- 3824Б1ПР5/16 Новожилов Данила Сергеевич/task1.c
+++ 3824Б1ПР5/16 Новожилов Данила Сергеевич/task1.c
@@ -1,6 +1,49 @@
-// Задача 1. Найти сумму целых чисел a и b.
-// Если a + b >= INT_MAX, возвращать INT_MAX
-// Если a + b <= INT_MIN, возвращать INT_MIN
+// Задача 1.a
+#include <stdio.h>
+#include <limits.h>
+
+int safe_sum(int a, int b) {
+    // Проверяем, есть ли риск переполнения
+    if ((a > 0 && b > INT_MAX - a) || (a < 0 && b < INT_MIN - a)) {
+        return (a > 0) ? INT_MAX : INT_MIN;
+    }
+    
+    return a + b;
+}
+
+int main() {
+    int a = 2000000000;
+    int b = 1000000000;
+    
+    int result = safe_sum(a, b);
+    
+    printf("Сумма %d и %d равна %d\n", a, b, result);
+    
+    return 0;
+}
+
+// Задача 1.b
+#include <stdio.h>
+#include <limits.h>
+
+int safe_sum(int a, int b) {
+    if ((a < 0 && b < INT_MIN - a) || (a > 0 && b < INT_MIN - a)) {
+        return INT_MIN;
+    }
+    
+    return a + b;
+}
+
+int main() {
+    int a = -2147483647;
+    int b = -10;
+    
+    int result = safe_sum(a, b);
+    
+    printf("Сумма %d и %d равна %d\n", a, b, result);
+    
+    return 0;
+}
 int sum_of_two_numbers(int a, int b)
 {
   return 0;
