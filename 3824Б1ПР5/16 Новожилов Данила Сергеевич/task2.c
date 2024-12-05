13e1866facddc2d6799ce7c4a5b4c6eb4a1f755a
dsnovozhylov <d9162589@gmail.com>
2024-12-05 10:15:06 +0300

    Update task2.c

--- 3824Б1ПР5/16 Новожилов Данила Сергеевич/task2.c
+++ 3824Б1ПР5/16 Новожилов Данила Сергеевич/task2.c
@@ -4,3 +4,26 @@ unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
   return 0;
 }
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+
+unsigned int product_of_two_numbers(unsigned int a, unsigned int b) {
+    // Проверим, можем ли мы безопасно умножить эти числа
+    if (a != 0 && b > UINT_MAX / a) {
+        return UINT_MAX;
+    }
+    
+    return a * b;
+}
+
+int main() {
+    unsigned int a = 4294967295U; // Максимальное значение для uint32_t
+    unsigned int b = 2;
+    
+    unsigned int result = product_of_two_numbers(a, b);
+    
+    printf("Произведение %u и %u равно %u\n", a, b, result);
+    
+    return 0;
+}
